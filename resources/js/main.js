20eaaadb68896c4c49dc07f3ca3ab26706f0c563
Kenneth Garcia <kennethgarcia12@hotmail.com>
2018-12-12 22:24:05 -0800

    fix some issues with footer responsiveness and added a little more content on the <p>.

---
+++ resources/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+/*  //place footer at bottom of page function
+  function setFooterHeight(){
+    //obtain necessary information
+    var footerPosition = $('#footer').offset();
+    var footerHeight = $('#footer').innerHeight();
+    var windowHeight = $(window).innerHeight();
+    var space_between = windowHeight - (footerPosition.top + footerHeight);
+    if($(window).width() > 760){
+      $('#footer').offset({top: footerPosition.top + space_between});
+    }
+  }
+
+//set footer's height normalscreen
+  setFooterHeight();
+
+//reset size if window is resized
+  $(window).resize(function(){
+    setFooterHeight();
+  });
+*/
+});//end of document
